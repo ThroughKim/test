dbbe7ae6807c109402acf13bef945e794e920989
luther.i <luther.i@kakaocorp.com>
2023-03-05 11:37:43 +0900

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by luther.i on 2023/03/05.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
